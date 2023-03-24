2a59c8eca48b76c4137d057eddc9f11e902ac2b0
mayanna estevanim <mayannaestevanim@gmail.com>
2023-03-24 20:27:24 +0100

    mogo atlas

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // URL local de nuestra base de datos en mongoose y su nombre
-const DB_URL = /* process.env.DB_URL ||  */'mongodb://localhost:27017/trabajo-final-grupo';
+const DB_URL = process.env.DB_URL ||  'mongodb://localhost:27017/trabajo-final-grupo';
 // Función que conecta nuestro servidor a la base de datos de MongoDB mediante mongoose
 const connect = () => mongoose.connect(DB_URL, {
   useNewUrlParser: true,
